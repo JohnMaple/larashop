d166a26c3380520e2853933edc2bd160e714f789
ethan <934879001@qq.com>
2020-05-11 17:26:39 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,1 +1,13 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+if (!function_exists('route_class')) {
+    /**
+     * 路由类转为css类名称
+     */
+    function route_class()
+    {
+        return str_replace('.', '-', Route::currentRouteName());
+    }
+}
